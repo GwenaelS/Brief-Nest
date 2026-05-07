4b9acbf9afb70d3c8f46f8fc96ee292005e9aaff
GwenaelS <samyngwenaelpro@gmail.com>
2026-05-07 16:50:06 +0200

    feat: add dotenv for environment variable management and create .env.sample

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,13 +1,16 @@
 import { DataSource } from 'typeorm';
 import { Init1778060668786 } from './migration/1778060668786-Init';
 import { User } from './modules/user/entities/user.entity';
+import dotenv from 'dotenv';
+
+dotenv.config()
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '1234',
-  database: 'briefNest',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [User],
   migrations: [Init1778060668786],
   synchronize: false,
